6f7a70dc5832e251e73d97990379d968a4b1413f
Malik22G <malik.basit3690@gmail.com>
2023-11-17 08:14:37 +0100

    Hndling form submission

--- issue-tracker/app/issues/new/page.tsx
+++ issue-tracker/app/issues/new/page.tsx
@@ -5,16 +5,46 @@ import classname from 'classnames';
 import { Button } from '@radix-ui/themes';
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form"
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+
+
+type IssueFormInputs = {
+  title: string
+  description: string
+}
 
 const newIssue = () => {
+  const router = useRouter()
+  const {
+    register,
+    control,
+    handleSubmit
+  } = useForm<IssueFormInputs>();
+  
+
   return (
-    <div className='max-w-xl space-y-3'>
+    <form className='max-w-xl space-y-3' 
+    onSubmit={handleSubmit(async (data)=> {
+    try{
+      await axios.post("/api/issues",data)
+      router.push('/issues')
+    }
+    catch(error){
+      console.log(error);
+    }
+  })}>
     <TextField.Root>
-    <TextField.Input placeholder="Title" />
+    <TextField.Input placeholder="Title" {...register("title")}/>
     </TextField.Root>
-    <SimpleMDE/>
+    <Controller
+    name = "description"
+    control = {control}
+    render = {({ field }) => <SimpleMDE placeholder='Description' {...field}/>}
+    />
     <Button>Submit New Isuue</Button>
-    </div>
+    </form>
   )
 }
 
